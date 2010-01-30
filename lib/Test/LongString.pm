b3deb2744af708062f284f08c9d8c86eb2f95b68
Rafael Garcia-Suarez <rgs@consttype.org>
2010-01-30 14:29:08 +0100

    Bump version to 0.13

--- lib/Test/LongString.pm
+++ lib/Test/LongString.pm
@@ -3,7 +3,7 @@ package Test::LongString;
 use strict;
 use vars qw($VERSION @ISA @EXPORT $Max $Context $LCSS);
 
-$VERSION = '0.12';
+$VERSION = '0.13';
 
 use Test::Builder;
 my $Tester = new Test::Builder();
